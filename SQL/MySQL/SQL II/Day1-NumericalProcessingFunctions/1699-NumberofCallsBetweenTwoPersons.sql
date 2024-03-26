30fc2c3b3ba9eb2ed6e8f65d39b66acd38b452a2
VEDITHARAO <37483060+VEDITHARAO@users.noreply.github.com>
2024-03-26 07:05:42 -0400

    Add files via upload

---
+++ SQL/MySQL/SQL II/Day1-NumericalProcessingFunctions/1699-NumberofCallsBetweenTwoPersons.sql
@@ -0,0 +1,82 @@
+/*
+Create table If Not Exists Calls (from_id int, to_id int, duration int)
+Truncate table Calls
+insert into Calls (from_id, to_id, duration) values ('1', '2', '59')
+insert into Calls (from_id, to_id, duration) values ('2', '1', '11')
+insert into Calls (from_id, to_id, duration) values ('1', '3', '20')
+insert into Calls (from_id, to_id, duration) values ('3', '4', '100')
+insert into Calls (from_id, to_id, duration) values ('3', '4', '200')
+insert into Calls (from_id, to_id, duration) values ('3', '4', '200')
+insert into Calls (from_id, to_id, duration) values ('4', '3', '499')
+*/
+
+/*
+Table: Calls
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| from_id     | int     |
+| to_id       | int     |
+| duration    | int     |
++-------------+---------+
+This table does not have a primary key, it may contain duplicates.
+This table contains the duration of a phone call between from_id and to_id.
+from_id != to_id
+ 
+
+Write an SQL query to report the number of calls and the total call duration between each pair of distinct persons (person1, person2) where person1 < person2.
+
+Return the result table in any order.
+
+The query result format is in the following example.
+
+Example 1:
+
+Input: 
+Calls table:
++---------+-------+----------+
+| from_id | to_id | duration |
++---------+-------+----------+
+| 1       | 2     | 59       |
+| 2       | 1     | 11       |
+| 1       | 3     | 20       |
+| 3       | 4     | 100      |
+| 3       | 4     | 200      |
+| 3       | 4     | 200      |
+| 4       | 3     | 499      |
++---------+-------+----------+
+Output: 
++---------+---------+------------+----------------+
+| person1 | person2 | call_count | total_duration |
++---------+---------+------------+----------------+
+| 1       | 2       | 2          | 70             |
+| 1       | 3       | 1          | 20             |
+| 3       | 4       | 4          | 999            |
++---------+---------+------------+----------------+
+Explanation: 
+Users 1 and 2 had 2 calls and the total duration is 70 (59 + 11).
+Users 1 and 3 had 1 call and the total duration is 20.
+Users 3 and 4 had 4 calls and the total duration is 999 (100 + 200 + 200 + 499).
+*/
+/*The MySQL UNION ALL operator is used to combine the result sets of 2 or more SELECT statements. 
+It returns all rows from the query and it does not remove duplicate rows between the various SELECT statements.
+Each SELECT statement within the MySQL UNION ALL operator must have the same number of fields in the result sets with similar data types.
+Syntax
+The syntax for the UNION ALL operator in MySQL is:
+SELECT expression1, expression2, ... expression_n
+FROM tables [WHERE conditions]
+UNION ALL
+SELECT expression1, expression2, ... expression_n
+FROM tables [WHERE conditions];*/
+
+--SOLUTION
+with cte as(
+    SELECT * from Calls WHERE from_id<to_id
+    UNION ALL
+    SELECT to_id as from_id,from_id as to_id,duration from calls 
+    WHERE from_id>to_id 
+)
+SELECT from_id as person1,to_id as person2,count(*) as call_count,sum(duration) as total_duration from cte 
+group by from_id,to_id
+ORDER by total_duration;
